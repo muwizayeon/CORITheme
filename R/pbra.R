f045df374c710655f8bd5fd009173ddb4c824348
Meongsu Lee <muwizayeon@mwc-119186.dhcp.missouri.edu>
2022-03-06 13:25:04 -0600

    update grain

---
+++ R/pbra.R
@@ -0,0 +1,24 @@
+#' Dataset used for creating example plots in the CORI style.
+#'
+#' @format Data frame with columns
+#' \describe{
+#' \item{row_id}{row id}
+#' \item{geoid}{}
+#' \item{rural_ruca}{}
+#' \item{rural_fhfa}{}
+#' \item{rural_forhp}{}
+#' \item{rural_rucc}{}
+#' \item{rural_uic}{}
+#' \item{rural_cdc}{}
+#' \item{rural_cbsa_2020}{}
+#' \item{rural_cbsa_2013}{}
+#' \item{rural_far}{}
+#' \item{year}{}
+#' \item{population}{}
+#' \item{race}{}
+#' \item{county}{}
+#' }
+"pbra"
+
+#' @importFrom tibble tibble
+NULL
